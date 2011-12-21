c15dea492060630ce62a922b184d2e9e5a498e23
Sergio de Mingo <sdemingo@gmail.com>
2011-12-21 18:43:32 +0100

    Last bugs found in io

--- examples/function.mk
+++ examples/function.mk
@@ -3,6 +3,11 @@
     # Codigo MK de alto nivel listo para ser compilado por mkc y
     # generar el codigo intermedio que ejecutará mkm 
 
+function resta(a,b)
+{
+	f=a+1
+	return f
+}
 
 function suma(a,b,c)
 {
@@ -13,5 +18,7 @@ function suma(a,b,c)
 function main()
 {
 	b=1
-	o=write('out',b)
+	c=5
+	o=resta(4,b)    # Me esta fallando si pongo esta funcion. Si la quito no
+	a=write('out',b)
 }
