e54af80c047eeda5346be04d33f16ba83f0b3a9a
Sergio de Mingo <sdemingo@gmail.com>
2012-03-09 23:00:21 +0100

    Add procs args in main

--- mkc/mkc.c
+++ mkc/mkc.c
@@ -5,14 +5,26 @@ extern FILE *out;
 
 int main (int argc, char **argv){
  
-  ++argv, --argc; /* se salta el nombre del programa */
-  if ( argc > 0 )
-    yyin = fopen( argv[0], "r" );
+  ++argv, --argc;
+  if (argv[0][0]!='-')
+    yyin = fopen(argv[0], "r" );
   else
     yyin = stdin;
 
-  out=fopen("/tmp/salida.il","w");
-  //  out=stdout;
+  while (argc>1){
+    if (argv[1][0]=='-'){
+      switch(argv[1][1]){
+      case 'o':
+	if (argv[2]!=NULL)
+	  out=fopen(argv[2],"w");
+      }
+    }
+    ++argv;
+    --argc;
+  }
+
+  if (out==NULL)
+    out=fopen("out.il","w");
  
   yyparse();
 
